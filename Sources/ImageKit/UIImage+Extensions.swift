92aa6a96faaf60d84c3516a2427b4be88ca0ea7a
alexpaul <alexpaulnyc@gmail.com>
2020-02-27 17:20:24 -0500

    working on updates

--- Sources/ImageKit/UIImage+Extensions.swift
+++ Sources/ImageKit/UIImage+Extensions.swift
@@ -47,19 +47,16 @@ extension UIImageView {
     
     // The UIActivityIndicatorView is used to indicate to the user that a download is in progress
     var activityIndicator: UIActivityIndicatorView!
-    DispatchQueue.main.async {
-      activityIndicator = UIActivityIndicatorView(style: .large)
-      activityIndicator = UIActivityIndicatorView(style: .large)
-      activityIndicator.color = UIColor.systemOrange
-      activityIndicator.startAnimating() // it's hidden until we explicitly start animating
-      
-      self.addSubview(activityIndicator) // we add the indicattor as a subview of the image view
-      
-      activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-      NSLayoutConstraint.activate([activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-                                   activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor)
-      ])
-    }
+    activityIndicator = UIActivityIndicatorView(style: .large)
+    activityIndicator.color = UIColor.systemOrange
+    activityIndicator.startAnimating() // it's hidden until we explicitly start animating
+    
+    self.addSubview(activityIndicator) // we add the indicattor as a subview of the image view
+    
+    activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                                 activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+    ])
     
     guard let url = URL(string: urlString) else {
       completion(.failure(.badURL(urlString)))
